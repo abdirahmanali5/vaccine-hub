3a93bdb3f1c3a5cb58acb9fcd2afba67a35b98ef
Abdirahman Ali <abdirahmanali@abdirah-ltmhqqy.internal.salesforce.com>
2022-07-07 11:30:07 -0700

    added sql files

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -13,7 +13,7 @@ app.use((req,res,next) => {
     return next(new NotFoundError())
 })
 app.use((err,req,res,next) => {
-    const status = err.status || 500
+    const status = err.status || 404
     const message = err.message
     return res.status(status).json({
         error: {message,status}
---
+++ vaccine-hub-api/vaccine-hub-schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+id          SERIAL PRIMARY KEY,
+password    TEXT NOT NULL,
+email       TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
+revp_status BOOLEAN NOT NULL,
+num_guests  INTEGER NOT NULL,
+created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+
